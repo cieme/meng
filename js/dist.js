e49a685adac41acb03df241e2ae2e6ec90098b02
Cieme <aslslgood22@163.com>
2019-08-19 10:30:37 +0800

    下拉

--- js/dist.js
+++ js/dist.js
@@ -4,6 +4,9 @@ $(function () {
             $(this).find(".book-li-bottom").slideToggle();
             $(this).find(".iconfont").toggleClass("icon-jianhao");
         })
+        $(".show_more_ul li").on("click", function () {
+            $(this).find(".zybox").slideToggle();
+        })
 
     } catch (error) {
 
